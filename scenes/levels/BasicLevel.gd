46b04fc1ceeb45c988129c3b9c49ca2ffbc02aa1
Olivier PEREZ <olivier@olivierperez.fr>
2024-06-22 17:41:07 +0200

    Go to next level, until the end

--- scenes/levels/BasicLevel.gd
+++ scenes/levels/BasicLevel.gd
@@ -3,6 +3,7 @@ class_name BasicLevel
 
 @export var start_point: Node2D
 @export var finish_area: Area2D
+@export var next_level: PackedScene
 
 func _ready():
 	var playerScene = preload("res://actors/Player.tscn")
@@ -16,6 +17,8 @@ func _ready():
 func _finish_entered(body):
 	if is_level_completed() and body is Player:
 		print("Level completed")
+		await get_tree().create_timer(1.5).timeout
+		get_tree().change_scene_to_packed(next_level)
 
 
 func is_level_completed() -> bool:
