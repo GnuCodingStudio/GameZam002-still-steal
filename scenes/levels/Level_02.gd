6ed316302db96c72cc084c4b2f6154282ecbce9d
Olivier PEREZ <olivier@olivierperez.fr>
2024-06-22 17:02:55 +0200

    Factorize levels to BasicLevel

--- scenes/levels/Level_02.gd
+++ scenes/levels/Level_02.gd
@@ -7,6 +7,5 @@ func _on_chest_on_opened():
 	chest_to_collect -= 1
 
 
-func _on_finish_area_entered(body):
-	if chest_to_collect <= 0:
-		print("Finished")
+func is_level_completed():
+	return chest_to_collect == 0
--- scenes/levels/Level_03.gd
+++ scenes/levels/Level_03.gd
@@ -1,8 +1,1 @@
 extends BasicLevel
-
-@onready var finish = %FinishArea
-
-
-func _on_body_entered(body):
-	if (body is Player):
-		print("Finish")
