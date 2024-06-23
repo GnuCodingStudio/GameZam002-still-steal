dda435d6ae767cbc148950b174f401923404d477
Olivier PEREZ <olivier@olivierperez.fr>
2024-06-23 11:34:39 +0200

    Show remaining actions count

--- controller/ActionController.gd
+++ controller/ActionController.gd
@@ -1,14 +1,23 @@
-extends Node
+extends CanvasLayer
 class_name ActionController
 
 
 @export_range(0, 100) var max_actions: int
 
 
+@onready var remaining_actions = %RemainingActions
+
+
+func _ready():
+	remaining_actions.text = str(max_actions)
+
 func activate() -> bool:
 	var can_activate = max_actions > 0
 	prints("Can activate", can_activate)
 	
-	max_actions -= 1
+	if max_actions >= 0:
+		max_actions -= 1
+		remaining_actions.text = str(max_actions)
+	
 	return can_activate
 
