392e5ec8992d100787d661cce9a0582710715b06
Olivier PEREZ <olivier@olivierperez.fr>
2024-06-23 16:41:06 +0200

    Make sound on chest opening

--- objects/WoodenChest.gd
+++ objects/WoodenChest.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var accessible_area: Area2D = %AccessibleArea
 @onready var animated_sprite: AnimatedSprite2D = %AnimatedSprite
+@onready var coin = %Coin
 
 signal on_opened()
 
@@ -20,5 +21,6 @@ func _try_activate():
 	var overlappingBodies = accessible_area.get_overlapping_bodies()
 	if overlappingBodies.any(func (elem): return elem is Player):
 		is_closed = false
+		coin.play()
 		animated_sprite.play("opened")
 		on_opened.emit()
