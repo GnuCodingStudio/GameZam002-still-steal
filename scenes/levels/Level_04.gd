3dea4bd21f2e2f54caebea9dafa7758c3043fffa
Olivier PEREZ <olivier@olivierperez.fr>
2024-06-23 17:01:01 +0200

    Fix Level 35

--- scenes/levels/Level_04.gd
+++ scenes/levels/Level_04.gd
@@ -3,8 +3,8 @@ extends BasicLevel
 @onready var intro: AudioStreamPlayer = %Intro
 
 func _init_level():
-	if IntrosController.can_play(4):
-		IntrosController.on_intro_played(4)
+	if IntrosController.can_play(5):
+		IntrosController.on_intro_played(5)
 		player.disabled = true
 		intro.play()
 		await intro.finished
