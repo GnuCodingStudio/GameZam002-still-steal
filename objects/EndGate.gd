d261c84e45041830a98edc308ef582a598a67298
Anthony MARQUET <anthony.marquet@zenika.com>
2024-07-23 00:51:12 +0200

    feat: add sound

--- objects/EndGate.gd
+++ objects/EndGate.gd
@@ -2,7 +2,9 @@ extends Node
 
 @onready var animated_sprite: AnimatedSprite2D = %AnimatedSprite2D
 @onready var collision: CollisionShape2D = %CollisionShape2D
-
+@onready var unlock_door = %Unlock_Door
 func _open():
 	animated_sprite.play("open")
 	collision.disabled = true
+	await get_tree().create_timer(0.5).timeout
+	unlock_door.play()
