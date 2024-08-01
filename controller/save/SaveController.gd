10036ffc0af017fecd6539730a738185eda15e1f
Olivier PEREZ <olivier@olivierperez.fr>
2024-08-01 13:47:28 +0200

    feat: Save to Json instead of Resource (#11)
    * feat: Save to Json instead of Resource
    
    * feat: remove useless global variable

--- controller/save/SaveController.gd
+++ controller/save/SaveController.gd
@@ -3,8 +3,7 @@ extends Node
 
 var save_file_path = "user://save/"
 
-var progression_file_name = save_file_path + "Progression.tres"
-var progression: ProgressionSave = ProgressionSave.new()
+var progression_file_name = save_file_path + "progression.json"
 
 
 func _ready():
@@ -12,12 +11,33 @@ func _ready():
 
 
 func load_progression() -> ProgressionSave:
-	var data = ResourceLoader.load(progression_file_name)
-	if data is ProgressionSave:
-		progression = data.duplicate(true)
-	return progression
+	if FileAccess.file_exists(progression_file_name):
+		var file = FileAccess.open(progression_file_name, FileAccess.READ)
+		var data = _parse(file.get_as_text())
+		file.close()
+		return data
+	else:
+		return ProgressionSave.new()
 
 
 func update_progression(scene: Node):
+	var progression = ProgressionSave.new()
 	progression.next_level = scene.scene_file_path
-	ResourceSaver.save(progression, progression_file_name, 0)
+	
+	var content = _serialize(progression)
+	var file = FileAccess.open(progression_file_name, FileAccess.WRITE)
+	file.store_string(content)
+	file.close()
+
+
+func _serialize(progression: ProgressionSave) -> String:
+	return JSON.stringify({
+		"next_level": progression.next_level
+	})
+
+
+func _parse(json: String) -> ProgressionSave:
+	var dict = JSON.parse_string(json)
+	var progression = ProgressionSave.new()
+	progression.next_level = dict["next_level"]
+	return progression
