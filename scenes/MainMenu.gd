28b08fdbe7ffce3cbd23c6fa0ca1ae11b5421bbd
Olivier PEREZ <olivier@olivierperez.fr>
2024-08-28 11:25:47 +0200

    feat(console): add a game console (#18)

--- scenes/MainMenu.gd
+++ scenes/MainMenu.gd
@@ -4,6 +4,7 @@ const first_level_file = "res://scenes/levels/Level_01.tscn"
 
 
 @onready var continue_button = %ContinueButton
+@onready var game_console = %GameConsole
 
 
 func _ready():
@@ -35,3 +36,31 @@ func _load_level(file: String):
 
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://scenes/credits/Credits.tscn")
+
+
+func _debug_invisible(target: String):
+	match(target):
+		"camera", "cameras":
+			DebugController.detectable_by_camera = false
+			game_console.log_info("Cameras disabled")
+		"guard", "guards":
+			DebugController.detectable_by_guard = false
+			game_console.log_info("Guards disabled")
+		"all":
+			DebugController.detectable_by_camera = false
+			DebugController.detectable_by_guard = false
+			game_console.log_info("Cameras and Guards disabled")
+		"none", "zero":
+			DebugController.detectable_by_camera = true
+			DebugController.detectable_by_guard = true
+			game_console.log_info("Cameras and Guards enabled")
+		_:
+			game_console.log_error("Invalid parameter")
+
+
+func _start_level(number: int):
+	var level_path = "res://scenes/levels/Level_%02d.tscn" % number
+	if ResourceLoader.exists(level_path):
+		_load_level(level_path)
+	else:
+		game_console.log_error("This level doesn't exist")
